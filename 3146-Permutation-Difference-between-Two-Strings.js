e3ed4a018c9d563a6306eedea9ab8310336a3ba4
Ahmed Essam <121862459+AESharak@users.noreply.github.com>
2025-01-06 19:08:48 +0200

    [3146] [Time Beats: 58.33%] [Memory Beats: 87.08%] - LeetPush

--- 3146-Permutation-Difference-between-Two-Strings.js
+++ 3146-Permutation-Difference-between-Two-Strings.js
@@ -4,14 +4,11 @@
  * @return {number}
  */
 var findPermutationDifference = function(s, t) {
-    var splittedS = s.split("");
-    var splittedT = t.split("");
     result = 0;
-    splittedS.forEach((char,idx)=>{
+    s.split("").forEach((char,idx)=>{
         if(t.includes(char)){
            result += Math.abs(idx - t.indexOf(char));
         }else{
-
         result += idx;
         }
     });
