50932255d7e315fbac46da107016ce3bf600e48e
Ahmed Essam <121862459+AESharak@users.noreply.github.com>
2025-01-08 08:49:29 +0200

    [9] [Time Beats: 19.70%] [Memory Beats: 80.35%] - LeetPush

---
+++ 9-Palindrome-Number.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function (x) {
+  if (x < 0) return false;
+  const reversedNo = +((x + \\)
+    .split(\\)
+    .map((n) => +n)
+    .reverse().join(\\));
+
+    if (+reversedNo === x) return true;
+    return false;
+};
