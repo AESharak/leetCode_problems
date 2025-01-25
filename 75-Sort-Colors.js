9dbbb89c6c45985e877ebebf3d9df19d5eb604ca
Ahmed Essam <121862459+AESharak@users.noreply.github.com>
2025-01-25 21:28:09 +0200

    [75] [Time Beats: 100.00%] [Memory Beats: 27.97%] - LeetPush

--- 75-Sort-Colors.js
+++ 75-Sort-Colors.js
@@ -3,20 +3,18 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function(nums) {
- let low = 0;
+    let low = 0; 
     let mid = 0;
-    let high = nums.length - 1;
-    
-    while(mid <= high) {
-        if(nums[mid] === 0) {
+    let high = nums.length -1;
+
+    while (mid <= high){
+        if (nums[mid] === 0){
             [nums[low], nums[mid]] = [nums[mid], nums[low]];
             low++;
             mid++;
-        }
-        else if(nums[mid] === 1) {
+        }else if (nums[mid] === 1){
             mid++;
-        }
-        else if(nums[mid] === 2) {
+        }else if (nums[mid] === 2){
             [nums[mid], nums[high]] = [nums[high], nums[mid]];
             high--;
         }
