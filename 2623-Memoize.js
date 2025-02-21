96d6fad2461e26bbe32dd036c3ad7c6fc4e4d911
Ahmed Essam <121862459+AESharak@users.noreply.github.com>
2025-02-21 17:30:36 +0200

    [2623] [Time Beats: 93.93%] [Memory Beats: 5.12%] - LeetPush

---
+++ 2623-Memoize.js
@@ -0,0 +1,30 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+function memoize(fn) {
+    const result = {};
+    return function(...args) {
+        const key = JSON.stringify(args);
+        if(key in result) {
+            return result[key];
+        }else {
+            result[key] = fn(...args);
+            return result[key]; 
+        }
+
+        
+    }
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
