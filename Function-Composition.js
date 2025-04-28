f200a9705b95b2904bb06e3b19d63e2476dc3f49
Ahmed Essam <121862459+AESharak@users.noreply.github.com>
2025-04-28 22:21:50 +0300

    [2629] [Time Beats: 59.78%] [Memory Beats: 92.93%] - LeetPush

--- Function-Composition.js
+++ Function-Composition.js
@@ -3,26 +3,15 @@
  * @return {Function}
  */
 var compose = function(functions) {
-    // Return a function that takes an argument x
     return function(x) {
-        // If empty array, return the identity function result
-        if (functions.length === 0) {
+        if (functions.length === 0){
             return x;
         }
-        
-        // Start with the initial value
-        let result = x;
-        
-        // Apply each function from right to left
-        for (let i = functions.length - 1; i >= 0; i--) {
-            result = functions[i](result);
-        }
-        
-        return result;
-    };
+       let result = x;
+       for (let i = functions.length - 1; i >= 0; i--){
+        result = functions[i](result);
+       }
+       return result;
+    }
 };
 
-/**
- * const fn = compose([x => x + 1, x => 2 * x])
- * fn(4) // 9
- */
