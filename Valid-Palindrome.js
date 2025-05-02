a2c605767e85cfba9bc89ff1523df64cdebe8e5e
Ahmed Essam <121862459+AESharak@users.noreply.github.com>
2025-05-02 21:04:56 +0300

    [125] [Time Beats: 75.51%] [Memory Beats: 38.39%] - LeetPush

--- Valid-Palindrome.js
+++ Valid-Palindrome.js
@@ -3,18 +3,48 @@
  * @return {boolean}
  */
 var isPalindrome = function (s) {
+  let start = 0;
+  let end = s.length - 1;
+  while (start < end) {
+    while (start < end && !isAlphanNumeric(s[start].toLowerCase())) {
+      start++;
+    }
+    while (start < end && !isAlphanNumeric(s[end].toLowerCase())) {
+      end--;
+    }
+    if (s[start].toLowerCase() !== s[end].toLowerCase()) return false;
+    start++;
+    end--;
+  }
+  return true;
+};
+
+function isAlphanNumeric(char) {
+  if (
+    (char.charCodeAt(0) >= 97 && char.charCodeAt(0) <= 122) ||
+    (char.charCodeAt(0) >= 48 && char.charCodeAt(0) <= 57) ||
+    char === \\
+  ) {
+    return true;
+  }
+  return false;
+}
+
+/*
+ an alternative with more space will be taken 
+ var isPalindromeMaxSpace = function (s) {
   let lowerCasedStr = s.toLowerCase();
   const arr = [];
   for (let i = 0; i < lowerCasedStr.length; i++) {
     if (
-     ( lowerCasedStr.charCodeAt(i) >= 97 &&
-      lowerCasedStr.charCodeAt(i) <= 122) ||
+      (lowerCasedStr.charCodeAt(i) >= 97 &&
+        lowerCasedStr.charCodeAt(i) <= 122) ||
       (lowerCasedStr.charCodeAt(i) >= 48 && lowerCasedStr.charCodeAt(i) <= 57)
     ) {
       arr.push(lowerCasedStr[i]);
     }
   }
- 
+
   const reversedArr = arr.slice().reverse();
 
   for (let index = 0; index < reversedArr.length; index++) {
@@ -24,3 +54,5 @@ var isPalindrome = function (s) {
   }
   return true;
 };
+
+*/
